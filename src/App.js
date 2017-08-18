0f46c8923e0a05c41ac46f96f88d1678b77f6cb2
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2017-08-19 00:33:23 +0530

    feat: Add jsfiddle code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,88 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+// DataList.js
+class DataList extends React.Component {
+	constructor(props) {
+  	super(props);
+    this.state = {
+    	dataSet: []
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+  	let { data } = nextProps;
+
+    let dataSet = this.pluckFieldList(data);
+    console.log('dataSet: ', dataSet);
+  	this.setState({ dataSet });
+  }
+
+  pluckFieldList(list) {
+
+    return list.reduce((obj, item) => {
+    	if (item in obj) {
+      	obj[item]++;
+      } else {
+      	obj[item] = 1;
+      }
+      return obj;
+    }, {});
+
+  }
+
   render() {
+  	console.log("List render: ", this.state);
+    let { dataSet } = this.state;
+    let keyArr = Object.keys(dataSet);
+  	return (
+    	<div>
+      	{keyArr.map(item => {
+        	return (
+          	<div key={item}>
+            	<input key={item} type="radio" name="selectedColor" value={item} />
+              <label style={{textTransform: 'capitalize'}}>{item} ({dataSet[item]})</label>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+  	super(props);
+    this.state = {
+    	data: [],
+      eyeColorsData: []
+    };
+
+  }
+
+  componentDidMount() {
+  	fetch('https://api.myjson.com/bins/17mcd1')
+    	.then(response => {
+      	return response.json()
+      })
+      .then(data => {
+        console.log('Data fetched: ', data);
+        let eyeColorsData = data.map(item => item.eyeColor);
+        this.setState({ data, eyeColorsData });
+
+      })
+      .catch(err => {
+      	console.log(err);
+      });
+  }
+
+  render() {
+  	let { eyeColorsData } = this.state;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    	<div>
+      	<DataList
+        	data={eyeColorsData} />
       </div>
     );
   }
